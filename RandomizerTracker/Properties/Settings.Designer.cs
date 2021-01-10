46f381ec430bedd5538d00e2c04a9526a65c8a40
flibber-hk <76987839+flibber-hk@users.noreply.github.com>
2021-01-10 20:42:13 +0000

    Add files via upload

--- RandomizerTracker/Properties/Settings.Designer.cs
+++ RandomizerTracker/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace RandomizerTracker.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.3.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.8.1.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -32,6 +32,18 @@ namespace RandomizerTracker.Properties {
             }
             set {
                 this["filepath"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string helperfilepath {
+            get {
+                return ((string)(this["helperfilepath"]));
+            }
+            set {
+                this["helperfilepath"] = value;
             }
         }
     }
